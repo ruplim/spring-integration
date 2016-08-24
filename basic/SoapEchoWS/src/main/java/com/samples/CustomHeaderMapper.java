e4e102ee5a59d7f34a0689a26a4d409d726ab27f
ruplim <ruplimbora@gmail.com>
2016-08-24 14:09:39 -0500

    SOAPEcho Updated

--- basic/SoapEchoWS/src/main/java/com/samples/CustomHeaderMapper.java
+++ basic/SoapEchoWS/src/main/java/com/samples/CustomHeaderMapper.java
@@ -7,11 +7,7 @@ import org.springframework.integration.ws.DefaultSoapHeaderMapper;
 import org.springframework.ws.soap.SoapHeader;
 import org.springframework.ws.soap.SoapHeaderElement;
 import org.springframework.ws.soap.SoapMessage;
-import org.springframework.ws.soap.saaj.SaajSoapHeaderException;
-import org.springframework.xml.transform.StringResult;
-import org.springframework.xml.transform.StringSource;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
+
 
 public class CustomHeaderMapper  extends DefaultSoapHeaderMapper {
 	
